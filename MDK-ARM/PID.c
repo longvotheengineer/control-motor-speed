6f1a275984e0651d56ebc4e27fbdc77acc711971
lvdaengineer <long.vo060204@hcmut.edu.vn>
2025-12-22 10:35:50 +0700

    style(): clean whitespace

--- MDK-ARM/PID.c
+++ MDK-ARM/PID.c
@@ -2,22 +2,22 @@
 
 volatile uint32_t  	enc_val_cur;
 volatile uint32_t 	enc_val_pre;
-volatile int16_t		enc_val_diff;
-int 	   		     	enc_dir;
-float				 	motor_speed;
-float				 	motor_output;
+volatile int16_t	enc_val_diff;
+int 	   		    enc_dir;
+float				motor_speed;
+float				motor_output;
 
 PID_Config motor_PID_config = 
 {
     .Kp 			= 0.0f,    
     .Ki 			= 0.0f,     
     .Kd 			= 0.0f,     
-    .T_sample 	= 0.01f, 
+    .T_sample 		= 0.01f, 
     .limMax 		= 130.0f, 
     .limMin 		= -130.0f, 
     .integrator 	= 0.0f,
-    .prevError 	= 0.0f,
-    .setpoint 	= 0.0f 
+    .prevError 		= 0.0f,
+    .setpoint 		= 0.0f 
 };
 
 void PID_Compute(PID_Config *pid) 
@@ -25,7 +25,7 @@ void PID_Compute(PID_Config *pid)
     float error = pid->setpoint - pid->measurement;
 	
     float proportional = pid->Kp * error;												
-    pid->integrator += pid->Ki * error * pid->T_sample; 					
+    pid->integrator   += pid->Ki * error * pid->T_sample; 					
 //    if (pid->integrator > pid->limMax) pid->integrator = pid->limMax;			
 //    else if (pid->integrator < pid->limMin) pid->integrator = pid->limMin;
 
@@ -34,7 +34,7 @@ void PID_Compute(PID_Config *pid)
     pid->out = proportional + pid->integrator + derivative;
 	
     if (pid->out > pid->limMax) pid->out 		= pid->limMax;
-    else if (pid->out < pid->limMin) pid->out  = pid->limMin;
+    else if (pid->out < pid->limMin) pid->out  	= pid->limMin;
 
     pid->prevError = error;
 }
