02b808c8b4c20adf59a794c2b311af20914bba3f
lvdaengineer <longdaicangu0002@gmail.com>
2025-12-27 08:50:11 +0700

    style(): clean whitespace

--- MDK-ARM/motor.h
+++ MDK-ARM/motor.h
@@ -6,7 +6,7 @@
 
 #define	MOTOR_ON			1
 #define	MOTOR_OFF			0
-#define	MOTOR_FORWARD	1
+#define	MOTOR_FORWARD		1
 #define	MOTOR_INVERSE		0
 
 
@@ -14,8 +14,8 @@ typedef struct
 {
 	bool 		enable;
 	bool		direction;
-	uint8_t	duty_cycle;		
-	uint8_t	speed;			
+	uint8_t		duty_cycle;		
+	uint8_t		speed;			
 	uint32_t	f_PWM;			
 }	MotorControl_t;
 
